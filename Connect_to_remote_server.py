8decd24ffd060763bd3a04f75683ff539b5d5bd1
Jacob Saleh <jacob.saleh@hpe.com>
2018-01-25 12:57:24 +0200

    first commit

--- Connect_to_remote_server.py
+++ Connect_to_remote_server.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 from sys import argv
 
-ip=argv[1]
-port=argv[2]
+if len(argv)>3 or len(argv)<2:
+    print("USAGE: command #IP #PORT (Port is optional, port 22 will be used by default)")
+    exit()
+
+ip_port=[]
+for i in range(1,len(argv)):
+    ip_port.append(argv[i])
+
+ip_port=tuple(ip_port)
+
 
 def ip_check(ip2ch):
     ip_split = ip2ch.split('.')
@@ -16,17 +24,29 @@ def port_check(port2ch):
     else:
         return False
 
+def connect_port(ip,port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(10)
+    result = sock.connect_ex((str(ip),int(port)))
+    if result == 0:
+        return True
+    else:
+        return False
+
 arguments=str(len(argv)-1)
 connectisok = {
-    '1': (ip_check(ip)),
-    '2': (ip_check(ip) and port_check(port))
+    '1': (ip_check(ip_port[0])),
+    '2': (ip_check(ip_port[0]) and port_check(ip_port[1]))
 }[arguments]
 
-def con_wo_port(): print('1st func')
+def con_wo_port(): print("Run connect_port(ip_port[0],'22') which is Run connect_port(%s,'22')") % (ip_port[0])
 
-def con_w_port(): print('2nd func')
+try:
+    def con_w_port(): print("Run connect_port(ip_port[0],ip_port[1]) which is Run connect_port(%s,%s)") % (ip_port[0],ip_port[1])
+except:
+    pass
 
 {
-'1': con_wo_port,
-'2': con_w_port
+    '1': con_wo_port,
+    '2': con_w_port
 }[arguments]()
